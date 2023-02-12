037d379a0e78e18de2ea33b3dd7ed18548708988
Anuj Kumar Sharma <anuj55149@gmail.com>
2023-02-12 18:34:07 +0530

    introduction to javascript

---
+++ learnjs/1. Introduction To Javascript/script.js
@@ -0,0 +1,17 @@
+let c = 12;
+let d = 13; 
+
+let age = 26; // initialisation
+
+const name = 'Anuj';
+const Name = "kumar";
+
+// console.log('age', age)
+// age = 28;
+
+// console.log("c+d is", c + d);
+
+// console.log('age', age)
+console.log('name', name);
+
+
