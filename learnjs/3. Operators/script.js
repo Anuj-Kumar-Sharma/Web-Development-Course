ae0d7c3ac3f59e3d2b8a1d88b966946fb353c9e2
Anuj Kumar Sharma <anuj55149@gmail.com>
2023-02-14 17:13:17 +0530

    operators

---
+++ learnjs/3. Operators/script.js
@@ -0,0 +1,17 @@
+const firstNumber = false;
+const secondNumber = true;
+
+// const result = firstNumber + secondNumber;
+// const result = firstNumber % secondNumber;
+const result = firstNumber || secondNumber;
+
+console.log(!secondNumber);
+
+// console.log(result);
+
+// 3 = 0*4 + 3
+
+// let thirdNumber = 12;
+// thirdNumber **= 4; // thirdNumber = thirdNumber + 4;
+
+// console.log(result);
