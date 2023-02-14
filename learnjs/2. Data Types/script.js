bb4f709d774e8bd77e0bb005c1801877d4ead438
Anuj Kumar Sharma <anuj55149@gmail.com>
2023-02-14 16:18:58 +0530

    data types in JS

---
+++ learnjs/2. Data Types/script.js
@@ -0,0 +1,20 @@
+let a = 23;
+// a = a+ 1;
+
+// console.log(a);
+
+// a = a+ 1;
+
+// console.log(a);
+
+// a = false
+
+a = Boolean(a);
+
+console.log("the value of a is", a);
+console.log("the type of a is", typeof a);
+
+let b = undefined;
+b = Number(b);
+
+console.log("b is ", b);
