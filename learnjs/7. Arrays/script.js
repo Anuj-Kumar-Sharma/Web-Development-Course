1622344cb89717d4ff994562ff9260d60ab80154
Anuj Kumar Sharma <anuj55149@gmail.com>
2023-02-28 16:25:54 +0530

    arrays

---
+++ learnjs/7. Arrays/script.js
@@ -0,0 +1,31 @@
+const nums = [4, 1, 2, 8, 2, 8, 6];
+
+// console.log(nums.indexOf(80));
+// console.log(nums.includes(80));
+
+// nums.sort();
+
+const subArray = nums.slice(2, 4);
+
+console.log(subArray);
+
+// const words = [
+//     "Apple",
+//     "Banana",
+//     "Cherry",
+//     12,
+//     true,
+//     {
+//         name: "Anuj",
+//     },
+//     function hello() {
+//         console.log("hello world");
+//     },
+// ];
+
+// const newWords = words;
+
+// newWords[6] = "Kiwi";
+
+// console.log(words);
+// console.log(newWords);
