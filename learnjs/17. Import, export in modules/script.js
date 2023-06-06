e3f2a3ff0677d1d0808b769a1041acee780cdd5b
Anuj Kumar Sharma <anuj55149@gmail.com>
2023-06-06 20:50:34 +0530

    import, exports in JS

---
+++ learnjs/17. Import, export in modules/script.js
@@ -0,0 +1,10 @@
+// import { mul, add } from "./utility.js";
+// import * as utility from './utility.js';
+
+import add from "./utility.js";
+
+
+const result = add(2, 3, 4);
+
+console.log(result);
+// console.log(utility.STUDENTS_COUNT);
---
+++ learnjs/17. Import, export in modules/utility.js
@@ -0,0 +1,13 @@
+function multiply(a, b, c) {
+    return a*b*c;
+}
+
+export default function addition(a, b, c) {
+    return a+b+c;
+}
+
+export const STUDENTS_COUNT = 23;
+
+export {
+    multiply as mul,
+}
